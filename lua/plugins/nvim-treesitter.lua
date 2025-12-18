ae7a34016d220436b3f79a475e216860236f368f
Chirag-Khandelwal <github@mail.chiragkh.me>
2025-12-18 12:05:34 +0530

    [Minor] Update treesitter config according to nvim-treesitter's rewrite

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,35 +1,82 @@
 return {
     "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter-context",
+    },
+    lazy = false,
     branch = "main",
-    main = "nvim-treesitter.config",
     build = ":TSUpdate",
-    opts = {
-        lazy = false,
-        auto_install = true,
-        indent = { enable = true, disable = { 'ruby' } },
-        highlight = {
-            enable = true,
-            -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-            --  If you are experiencing weird indenting issues, add the language to
-            --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-            additional_vim_regex_highlighting = { 'ruby' }
-        },
-        ensure_installed = {
+    config = function()
+        local ts = require("nvim-treesitter")
+
+        -- Install core parsers at startup
+        ts.install({
             "bash",
             "c",
             "cpp",
+            "comment",
             "fish",
+            "git_config",
+            "git_rebase",
+            "gitcommit",
+            "gitignore",
+            "javascript",
             "json",
+            "latex",
             "lua",
+            "luadoc",
+            "make",
             "markdown",
             "markdown_inline",
+            "norg",
             "python",
             "query",
             "regex",
+            "toml",
             "tsx",
             "typescript",
+            "typst",
             "vim",
+            "vimdoc",
+            "vue",
+            "xml",
             "yaml",
+        }, {
+            max_jobs = 8,
+        })
+
+        local group = vim.api.nvim_create_augroup("TreesitterSetup", { clear = true })
+
+        local ignore_filetypes = {
+            "checkhealth",
+            "lazy",
+            "mason",
+            "snacks_dashboard",
+            "snacks_notif",
+            "snacks_win",
         }
-    }
+
+        -- Auto-install parsers and enable highlighting on FileType
+        vim.api.nvim_create_autocmd("FileType", {
+            group = group,
+            desc = "Enable treesitter highlighting and indentation",
+            callback = function(event)
+                if vim.tbl_contains(ignore_filetypes, event.match) then
+                    return
+                end
+
+                local lang = vim.treesitter.language.get_lang(event.match) or event.match
+                local buf = event.buf
+
+                -- Start highlighting immediately (works if parser exists)
+                pcall(vim.treesitter.start, buf, lang)
+
+                -- Enable treesitter indentation
+                vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+
+                -- Install missing parsers (async, no-op if already installed)
+                ts.install({ lang })
+            end,
+        })
+    end,
 }
